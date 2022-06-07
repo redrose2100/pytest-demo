2f93d39e2a721ddd7b619fab56535d9fd8036a58
redrose2100 <redrose2100@163.com>
2022-06-08 00:09:32 +0800

    add ex_001 and ex_002

---
+++ ch02/ex_001/test_demo.py
@@ -0,0 +1,24 @@
+
+def test_demo01():
+    assert 0
+
+def test_demo02():
+    assert 0.0
+
+def test_demo03():
+    assert ""
+
+def test_demo04():
+    assert ()
+
+def test_demo05():
+    assert {}
+
+def test_demo06():
+    assert []
+
+def test_demo07():
+    assert None
+
+def test_demo08():
+    assert False
---
+++ ch02/ex_002/test_demo.py
@@ -0,0 +1,15 @@
+
+def test_demo01():
+    assert 1==2
+
+def test_demo02():
+    assert 3<2
+
+def test_demo03():
+    assert 3 is str
+
+def test_demo04():
+    assert 3 in [1,2,4]
+
+def test_demo05():
+    assert 0 is None
