b9f2fb400fe440fdd5510ea1dcba79bf9541eff2
redrose2100 <redrose2100@163.com>
2022-06-10 22:55:11 +0800

    update

---
+++ ch03/ex_001/test_demo.py
@@ -0,0 +1,41 @@
+
+
+def test_func1():
+    print("test_func1")
+    assert 1==1
+
+def testfunc2():
+    print("testfunc2")
+    assert 1==1
+
+def Test_func3():
+    print("Test_func3")
+    assert 1==1
+
+def Testfunc4():
+    print("Testfunc4")
+    assert 1==1
+
+def func5_test():
+    print("func5_test")
+    assert 1==1
+
+def func6test():
+    print("func6test")
+    assert 1==1
+
+def func7_Test():
+    print("func7_Test")
+    assert 1==1
+
+def func8Test():
+    print("func8Test")
+    assert 1==1
+
+def TESTfunc9():
+    print("TESTfunc9")
+    assert 1==1
+
+def TEST_func10():
+    print("TEST_func10")
+    assert 1==1
