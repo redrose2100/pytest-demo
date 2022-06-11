9319cfaed1f80f0dad41fa301f73d79be3a427a0
redrose2100 <redrose2100@163.com>
2022-06-11 12:33:56 +0800

    add demo

---
+++ ch03/ex_002/test_demo.py
@@ -0,0 +1,27 @@
+
+class TestDemo(object):
+    def test_demo(self):
+        print("in TestDemo ...")
+        assert 1==1
+
+class testDemo(object):
+    def test_demo(self):
+        print("in testDemo ...")
+        assert 1==1
+
+class DemoTest(object):
+    def test_demo(self):
+        print("in DemoTest ...")
+        assert 1==1
+
+class Demo(object):
+    def test_demo(self):
+        print("in Demo ...")
+        assert 1==1
+
+class Test2Demo(object):
+    def __init__(self):
+        pass
+    def test_demo(self):
+        print("in Test2Demo ...")
+        assert 1==1
