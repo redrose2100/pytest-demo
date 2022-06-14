6e5ddd314568e4df4f9601525d8161b97d43b061
redrose2100 <redrose2100@163.com>
2022-06-14 22:27:27 +0800

    update:

---
+++ ch03/ex_012/test_demo.py
@@ -0,0 +1,14 @@
+
+def setup_module():
+    print("in setup_module ...")
+
+def teardown_module():
+    print("in teardown_module ...")
+
+def test_01():
+    print("in test_01 ...")
+    assert 1==1
+
+def test_02():
+    print("in test_02 ...")
+    assert 1==1
