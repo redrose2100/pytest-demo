6cfb2e1c056351480710dd05b0d214930a2427a5
redrose2100 <redrose2100@163.com>
2022-06-15 00:19:56 +0800

    add ex

---
+++ ch03/ex_define_file_class_function_name_rule/test_demo.py
@@ -0,0 +1,20 @@
+def test_demo():
+    assert 1==1
+
+def demo_test():
+    assert 1==1
+
+class TestDemo(object):
+    def test_demo(self):
+        assert 1==1
+
+    def demo_test(self):
+        assert 1==1
+
+
+class DemoTest(object):
+    def test_demo(self):
+        assert 1 == 1
+
+    def demo_test(self):
+        assert 1 == 1
