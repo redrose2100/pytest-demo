d18af56e6e52b1aa626f7273686727b6d7f56d0c
redrose2100 <redrose2100@163.com>
2022-06-18 15:54:39 +0800

    update

---
+++ ch03/ex_017/demo04/test_demo01.py
@@ -0,0 +1,12 @@
+import sys
+
+print("in test_demo01 ...")
+for elem in sys.path:
+    if "pytest-demo" in elem:
+        print("sys.path:" + elem)
+for elem in sys.modules.keys():
+    if "demo" in elem:
+        print("module:"+elem)
+
+def test_func():
+    assert 1==1
