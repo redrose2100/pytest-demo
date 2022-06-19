3e47e25fb8c8d48d749e2745f492c7ff3db7c1a2
redrose2100 <redrose2100@163.com>
2022-06-19 10:34:36 +0800

    update

---
+++ ch04/ex_002/test_demo.py
@@ -0,0 +1,9 @@
+
+class TestDemo(object):
+    def test_01(self):
+        print("in TestDemo.test_01 ...")
+        assert 1==1
+
+    def test_02(self):
+        print("in TestDemo.test_02 ...")
+        assert 1==1
